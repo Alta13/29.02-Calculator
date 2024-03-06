0d3c2b36e98ee8a9262ecd6b5eb34cbe201eb873
ПК <@>
2024-03-06 10:49:36 +0300

    Добавьте файлы проекта.

---
+++ 29.02 Calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+    class Calculator
+    {
+        static void Main()
+        {
+            double num1, num2, result;
+            char option;
+
+            Console.WriteLine("Введите первое число:");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите действие (+, -, *, /):");
+        option = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            switch (option)
+            {
+                case '+':
+                    result = num1 + num2;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    Console.WriteLine("Результат: " + result);
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine("Результат: " + result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка! Делить на 0 нельзя!");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Ошибка! Выбрано не верное действие!");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+
---
+++ 29.02 Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("29.02 Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("29.02 Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1da90e34-94db-4d81-8ffc-70f6b64357b2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
